7232f5cb6bdca11de8faeb4706d8dc34aa8e16e9
Matthew Lane <themplane@gmail.com>
2020-09-16 18:20:47 -0700

    Create Trip Class and Test File

---
+++ test/Trip-test.js
@@ -0,0 +1,55 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Trip from '../src/Trip.js';
+
+describe('Trip', () => {
+  beforeEach(() => {
+    let fdata = {
+      "id": 1,
+      "userID": 44,
+      "destinationID": 49,
+      "travelers": 1,
+      "date":"2019/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities":[]
+    };
+
+    let trip = new Trip(fdata);
+  });
+
+  describe('Initialization', () => {
+
+    it('should store an id', () => {
+      expect(trip.id).to.equal(fdata.id);
+    });
+
+    it('should store an userID', () => {
+      expect(trip.userID).to.equal(fdata.userID);
+    });
+
+    it('should store an destinationID', () => {
+      expect(trip.destinationID).to.equal(fdata.destinationID);
+    });
+
+    it('should store an travelers', () => {
+      expect(trip.travelers).to.equal(fdata.travelers);
+    });
+
+    it('should store an date', () => {
+      expect(trip.date).to.equal(fdata.date);
+    });
+
+    it('should store an duration', () => {
+      expect(trip.duration).to.equal(fdata.duration);
+    });
+
+    it('should store an suggestedActivities', () => {
+      expect(trip.suggestedActivities).to.deep.equal(fdata.suggestedActivities);
+    });
+
+    it('should store an status', () => {
+      expect(trip.status).to.equal(fdata.status);
+    });
+  });
+});
