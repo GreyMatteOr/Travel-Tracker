b9dc7f304f408583294e512dd89922d7c7196459
Matthew Lane <themplane@gmail.com>
2020-09-22 11:59:24 -0700

    CSS Styling...

--- src/domscripts.js
+++ src/domscripts.js
@@ -6,13 +6,18 @@ let domscripts = {
     let end = time.daysFromDate(trip.date, trip.duration).toString().slice(0, 15);
     return `
       <div class='trip-card' data-value="${trip.id}">
-        <img src='${trip.destination.image}' alt='${trip.destination.alt}/>
+        <img src='${trip.destination.image}' alt='${trip.destination.alt}'/>
         <div class='card-text'>
-          <h2 class='main-font'>${trip.getName()}<h2>
-          <h3 class='main-font'>Departure Date: ${start}<h3>
-          <h3 class='main-font'>Return Date: ${end}<h3>
-          <h3 class='main-font'>Status:
-          ${isUpcoming ? `<button class=\'main-font destroy-card\'>Cancel</button>` : ''}
+          <h1 class='main-font'>${trip.getName()}</h1>
+          <h3 class='main-font'>Flights and lodging for: ${trip.travelers}</h3>
+          <h3 class='main-font'>Departure Date: ${start}</h3>
+          <h3 class='main-font'>Return Date: ${end}</h3>
+          ${isUpcoming
+            ? `
+            <h3 class='main-font'>Status: ${trip.status}</h3>
+            <button class=\'main-font destroy-card\'>Cancel</button>
+            `
+            : ''}
         </div>
       </div>
     `
